ebac672f1d02acbf49177e7a0c3d5eaf197c8c90
hoka-sp <ohisama365.not@gmail.com>
2022-03-06 15:54:47 +0900

    レベル要素の追加

--- hoka/createGames/shootingGame/src/mainPackage/Shooting.java
+++ hoka/createGames/shootingGame/src/mainPackage/Shooting.java
@@ -25,19 +25,18 @@ public class Shooting {
 		int FPS = 0;
 		int FPSCount = 0;
 		Graphics gra = shootingFrame.panel.image.getGraphics();
-		
+		EnumShootingScreen screen = EnumShootingScreen.START;
+
 		//GAME
 		int playerX = 0, playerY = 0;
 		int bulletInterval = 0;
 		int score = 0;
+		int level = 0;
+		long levelTimer = 0;
 		ArrayList<Bullet> bullets_player = new ArrayList<>();
 		ArrayList<Bullet> bullets_enemy = new ArrayList<>();
 		ArrayList<Enemy> enemies = new ArrayList<>();
 		Random random = new Random();
-		
-		
-		EnumShootingScreen screen = EnumShootingScreen.START;
-		
 		
 		
 		while(loop) {
@@ -72,10 +71,18 @@ public class Shooting {
 					playerX = 235;
 					playerY = 400;
 					score = 0;
+					level = 0;
+					
 				}
 				
 				break;
 			case GAME:
+				if (System.currentTimeMillis() - levelTimer > 10 * 1000) {
+					levelTimer = System.currentTimeMillis();
+					level ++;
+				}
+				
+				
 				gra.setColor(Color.BLUE);
 				gra.fillRect(playerX + 10, playerY, 10, 10);
 				gra.fillRect(playerX, playerY + 10, 30, 10);
@@ -114,14 +121,17 @@ public class Shooting {
 					
 					enemy.y += 3;
 					if(enemy.y > 500) enemies.remove(i);
-					if(random.nextInt(80)== 1) bullets_enemy.add(new Bullet(enemy.x, enemy.y));
+					if(random.nextInt(level<50?80 - level:30)== 1) bullets_enemy.add(new Bullet(enemy.x, enemy.y));
 					if((enemy.x >= playerX && enemy.x <= playerX + 30 &&
 							enemy.y >= playerY && enemy.y <= playerY + 20)||
 						(enemy.x + 30 >= playerX && enemy.x + 30 <= playerX + 30 &&
 							enemy.y + 20 >= playerY && enemy.y + 20 <= playerY + 20)) {
 							screen = EnumShootingScreen.GAME_OVER;
+							score += (level - 1) * 100;
 						}
 					}
+				
+				if(random.nextInt(level<10?30 - level:30)== 1) enemies.add(new Enemy(random.nextInt(450), 0));
 				
 				for(int i = 0; i < bullets_enemy.size();i++) {
 					Bullet bullet = bullets_enemy.get(i);
@@ -133,10 +143,9 @@ public class Shooting {
 					if(bullet.x >= playerX && bullet.x <= playerX + 30 &&
 						bullet.y >= playerY && bullet.y <= playerY + 20) {
 						screen = EnumShootingScreen.GAME_OVER;
+						score += (level - 1) * 100;
 					}
 				}
-				
-				if(random.nextInt(30)== 1) enemies.add(new Enemy(random.nextInt(450), 0));
 				
 				if(Keyboard.isKeyPressed(KeyEvent.VK_LEFT) && playerX>0) playerX-=5;
 				if(Keyboard.isKeyPressed(KeyEvent.VK_RIGHT) && playerX<455) playerX+=5;
@@ -153,7 +162,9 @@ public class Shooting {
 				
 				gra.setColor(Color.BLACK);
 				gra.setFont(new Font("SansSerif", Font.PLAIN, 20));
-				gra.drawString("SCORE:" + score, 10, 30);
+				gra.drawString("SCORE:" + score, 10, 20);
+				gra.drawString("LEVEL:" + level, 10, 40);
+				
 				break;
 			case GAME_OVER:
 				gra.setColor(Color.BLACK);
