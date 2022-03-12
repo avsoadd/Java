5c6c51c47611f5d7854fa9863c49fd59ff071e6c
hoka-sp <ohisama365.not@gmail.com>
2022-03-12 17:54:41 +0900

    databaseプロジェクトの作成

---
+++ hoka/createDatabase/database/src/main/java/com/sus/database/webUIController.java
@@ -0,0 +1,16 @@
+package com.sus.database;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class webUIController {
+	
+	@RequestMapping(path = "/", method = RequestMethod.GET)
+	public String index(HttpServletResponse response) throws Exception {
+		return "home";
+		}
+}
