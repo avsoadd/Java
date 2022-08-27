24aef9b3e7ef36040c67e69fbb723a336d2853de
kanzaki-t <kanzakifei0612@gmail.com>
2022-08-27 18:10:51 +0900

    jankan

---
+++ kanzaki/jan/src/jan/Check.java
@@ -0,0 +1,15 @@
+package jan;
+
+public class Check {
+	public static void check () {
+		
+	}
+	/*	public static void main(String[] args) {
+			// TODO 自動生成されたメソッド・スタブ
+			char userParts ;
+			// userParts ="";
+			char cpuParts;
+			cpuParts =;
+		}*/
+
+}
---
+++ kanzaki/jan/src/jan/Jankenmain.java
@@ -0,0 +1,18 @@
+package jan;
+
+public class Jankenmain {
+	public void show() {
+		System.out.println("hello");
+	}
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Jankenmain.show2();
+		Jankenmain jmain = new Jankenmain();
+		jmain.show();
+		Check data = new Check();
+		Check.check();
+	}
+	public static void show2() {
+		System.out.println("yo");
+	}
+}
