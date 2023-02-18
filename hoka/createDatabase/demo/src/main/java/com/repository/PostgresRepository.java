a8d7db9f948d2e23082036f91efc605a4678421d
hoks-sp <oka-h@m.sus-g.co.jp>
2023-02-18 18:06:40 +0900

    20230218勉強会のコミット

---
+++ hoka/createDatabase/demo/src/main/java/com/repository/PostgresRepository.java
@@ -0,0 +1,29 @@
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+@Table("customers")
+public class Customer {
+    @Id
+    @Column("customer_id")
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    @Column("created_at")
+    private LocalDateTime createdAt;
+    @Column("updated_at")
+    private LocalDateTime updatedAt;
+
+    // getters and setters
+}
+
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    @Query("SELECT * FROM customers WHERE email = :email")
+    Customer findByEmail(String email);
+}
