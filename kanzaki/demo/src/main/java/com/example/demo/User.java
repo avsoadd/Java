a1bd50f06e1685a675046873603a1cab9cc73272
kanzaki-t <kanzakifei0612@gmail.com>
2023-03-18 17:49:57 +0900

    3-18

--- kanzaki/demo/src/main/java/com/example/demo/User.java
+++ kanzaki/demo/src/main/java/com/example/demo/User.java
@@ -6,14 +6,10 @@ import lombok.Data;
 
 @Data
 public class User {
-
-	private String name;
-
-	private Integer age;
-
 	private Integer userHand;
 
-	private Integer npcHand;
+	private Integer npc1Hand;
+	private Integer npc2Hand;
 
 	public String getUserhandString() {
 		if (this.userHand == 1) {
@@ -27,29 +23,104 @@ public class User {
 		}
 	}
 
-	public String getNpchandString() {
+	public String getNpc1handString() {
 
 		Random random = new Random();
-		this.npcHand = random.nextInt(3)+1;
+		this.npc1Hand = random.nextInt(3) + 1;
 
-		if (this.npcHand == 1) {
+		if (this.npc1Hand == 1) {
 			return "グー";
-		} else if (this.npcHand == 2) {
+		} else if (this.npc1Hand == 2) {
 			return "チョキ";
-		} else if (this.npcHand == 3) {
+		} else if (this.npc1Hand == 3) {
 			return "パー";
 		} else {
 			return "エラー";
 		}
 	}
 
-    public String result(Integer userHand, Integer npcHand) {
-        if (userHand == npcHand) {
-            return "引き分け"; 
-        } else if ((userHand == 1 && npcHand == 2) || (userHand == 2 && npcHand == 3) || (userHand == 3 && npcHand == 1)) {
-            return "userの勝利";
-        } else {
-            return "npcの勝利"; 
-        }
-    }
+	public String getNpc2handString() {
+		Random random = new Random();
+		this.npc2Hand = random.nextInt(3) + 1;
+		if (this.npc2Hand == 1) {
+			return "グー";
+		} else if (this.npc2Hand == 2) {
+			return "チョキ";
+		} else if (this.npc2Hand == 3) {
+			return "パー";
+		} else {
+			return "エラー";
+		}
+	}
+
+	public String result(Integer userHand, Integer npc1Hand, Integer npc2Hand) {
+		switch (userHand) {
+		case 1:
+			if (userHand == npc1Hand) {
+				if (npc2Hand == 3) {
+					return "npc2の勝利";
+				} else {
+					return "引き分け";
+				}
+			} else if (userHand == npc2Hand) {
+				if (npc1Hand == 3) {
+					return "npc1の勝利";
+				} else {
+					return "引き分け";
+				}
+			} else if (npc1Hand == npc2Hand) {
+				if (npc1Hand == 2) {
+					return "userの勝利";
+				} else {
+					return "引き分け";
+				}
+			}
+			break;
+		case 2:
+			if (userHand == npc1Hand) {
+				if (npc2Hand == 1) {
+					return "npc2の勝利";
+				} else {
+					return "引き分け";
+				}
+			} else if (userHand == npc2Hand) {
+				if (npc1Hand == 1) {
+					return "npc1の勝利";
+				} else {
+					return "引き分け";
+				}
+			} else if (npc1Hand == npc2Hand) {
+				if (npc1Hand == 3) {
+					return "userの勝利";
+				} else {
+					return "引き分け";
+				}
+			}
+			break;
+		case 3:
+			if (userHand == npc1Hand) {
+				if (npc2Hand == 2) {
+					return "npc2の勝利";
+				} else {
+					return "引き分け";
+				}
+			} else if (userHand == npc2Hand) {
+				if (npc1Hand == 2) {
+					return "npc1の勝利";
+				} else {
+					return "引き分け";
+				}
+			} else if (npc1Hand == npc2Hand) {
+				if (npc1Hand == 1) {
+					return "userの勝利";
+				} else {
+					return "引き分け";
+				}
+			}
+			break;
+		default:
+			return "未知";
+		}
+		return "引き分け";
+	}
 }
