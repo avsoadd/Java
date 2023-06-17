285c83517ebe56f2d00f283ea20cbb4edd89d1ea
fujita-kai <fujita-ka@m.sus-g.co.jp>
2023-06-17 17:47:24 +0900

    a

--- fujita-kai/OthelloGame/src/Main.java
+++ fujita-kai/OthelloGame/src/Main.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 public class Main {
 	static int ban[][] = { 
 			{ 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -6,27 +6,12 @@ public class Main {
 			{ 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 0, 0 } };
 	static boolean putStone[][] = { 
 			{ false, false, false, false, false, false, false, false }, { false, false, false, false, false, false, false, false },
-			{ false, false, false, false, false, false, false, false }, { false, false, false, false, false, false, false, false },
-			{ false, false, false, false, false, false, false, false }, { false, false, false, false, false, false, false, false },
+			{ false, false, false, false, false, false, false, false }, { false, false, false, true, true, false, false, false },
+			{ false, false, false, true, true, false, false, false }, { false, false, false, false, false, false, false, false },
 			{ false, false, false, false, false, false, false, false }, { false, false, false, false, false, false, false, false } };
 
 
 	public static void main(String[] args) {
-		
-	}
-	public void check(int color , int pP[]) {
-		int anColor = 0;
-		if(color == 1) {
-			anColor = 2;
-		}else {
-			anColor = 1;
-		}
-		if(ban[pP[0]][pP[1]] == 0) {
-			if(ban[pP[0]-1][pP[1]-1] == anColor || ban[pP[0]-1][pP[1]] == anColor || ban[pP[0]-1][pP[1]+1] == anColor ||
-					ban[pP[0]][pP[1]-1] == anColor || ban[pP[0]][pP[1]+1] == anColor ||
-					ban[pP[0]+1][pP[1]-1] == anColor || ban[pP[0]+1][pP[1]] == anColor || ban[pP[0]+1][pP[1]+1] == anColor) {
-				putStone[pP[0]][pP[1]] = true;
-			}
-		}
+		ArrayList<Boolean> reverses[];
 	}
 }
