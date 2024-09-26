603ee61f87f7aab2530e6eb733961383690fc110
Naoki3588 <138868369+Naoki3588@users.noreply.github.com>
2024-09-26 18:10:08 +0900

    schooにて条件分岐の演習

---
+++ furuya-n/java_practice/src/practice/Sample4_1.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class Sample4_1 {
+
+	public static void main(String[] args) {
+		
+		int x = 10;
+		if(x>=10) {
+			 System.out.println("Hello!");
+		}
+        
+		double y = 99;
+		if(y< 100) {
+			System.out.println("Hello!");
+		}
+	}
+}
