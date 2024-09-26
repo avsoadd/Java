c5a8b254babdfa0c8d14816b2ba2b06ed6853227
Naoki3588 <138868369+Naoki3588@users.noreply.github.com>
2024-09-26 14:22:05 +0900

    テスト

---
+++ work2/src/work2/Work3.java
@@ -0,0 +1,10 @@
+package work2;
+
+class Work3 {
+
+	public static void main(String[] args) {
+		
+        System.out.println("HelloWorld!!");
+	}
+
+}
---
+++ work2/src/work2/Work4.java
@@ -0,0 +1,10 @@
+package work2;
+
+public class Work4 {
+
+	public static void main(String[] args) {
+		
+        System.out.println(1+3);
+	}
+
+}
