3bc402e934ba5b44c5e8e0c7fa5a66880023df01
Naoki3588 <138868369+Naoki3588@users.noreply.github.com>
2024-09-27 11:17:49 +0900

    schoo④条件分岐の演習

--- furuya-n/java_practice/src/practice/Sample4_1.java
+++ furuya-n/java_practice/src/practice/Sample4_1.java
@@ -3,15 +3,15 @@ package practice;
 public class Sample4_1 {
 
 	public static void main(String[] args) {
-		
-		int x = 10;
-		if(x>=10) {
-			 System.out.println("Hello!");
+		int x = 49;
+		if(x>=100) {
+			System.out.println("100以上です");
 		}
-        
-		double y = 99;
-		if(y< 100) {
-			System.out.println("Hello!");
+		else if(x>=50) {
+			System.out.println("50以上,100未満です");
+		}
+		else{
+			System.out.println("それ以外です");
 		}
 	}
 }
