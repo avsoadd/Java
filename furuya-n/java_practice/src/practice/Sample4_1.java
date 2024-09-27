b48b6bbaca9147fe7632b87c43738388946168b5
Naoki3588 <138868369+Naoki3588@users.noreply.github.com>
2024-09-27 17:35:05 +0900

    schooにてjava入門初級編④条件分岐演習

--- furuya-n/java_practice/src/practice/Sample4_1.java
+++ furuya-n/java_practice/src/practice/Sample4_1.java
@@ -3,15 +3,25 @@ package practice;
 public class Sample4_1 {
 
 	public static void main(String[] args) {
-		int x = 49;
-		if(x>=100) {
-			System.out.println("100以上です");
+		int x = 5+5;
+		if(x >=10) {
+			System.out.println("10以上です");
 		}
-		else if(x>=50) {
-			System.out.println("50以上,100未満です");
+		
+		int y = 100/20;
+		if(y >=10) {
+			System.out.println("10以上です");
 		}
-		else{
-			System.out.println("それ以外です");
+		else {
+			System.out.println("10未満です");
+		}
+		
+		int z = 10 % 3;
+		if(z == 0 ) {
+			System.out.println("割り算のあまりは0です");
+		}
+		else {
+			System.out.println("割り算のあまりは" + z + "です");
 		}
 	}
 }
